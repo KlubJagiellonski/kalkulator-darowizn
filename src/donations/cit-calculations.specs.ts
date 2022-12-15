41888ca8e619faa94a8126275bdb411297764108
Adam Walkowski <adam.walkowski@inventcore.net>
2022-12-15 22:54:55 +0100

    updated tests

--- src/donations/cit-calculations.specs.ts
+++ src/donations/cit-calculations.specs.ts
@@ -3,7 +3,12 @@ import { testCase } from 'src/utils/test-case';
 import { calculateForCIT2022 } from './cit-calculations';
 
 describe('CIT 2022 calculations for annual income:', () => {
-    [testCase(1, 0, 0), testCase(54000, 5400, 486), testCase(6000000, 600000, 114000)].forEach((test) => {
+    [
+        testCase(1, 0, 0),
+        testCase(54000, 5400, 486),
+        testCase(54000, 5400, 486),
+        testCase(12 * 500000, 600000, 114000),
+    ].forEach((test) => {
         it(`${test.annualIncome} zł`, () => {
             const result = calculateForCIT2022(test.annualIncome);
             expect(result.donationSum).equals(test.donationSum, 'incorrect donation sum');
--- src/donations/pit-calculations-2022.specs.ts
+++ src/donations/pit-calculations-2022.specs.ts
@@ -4,10 +4,21 @@ import { calculateForPIT2022 } from './pit-calculations-2022';
 
 describe('PIT 2022 calculations for annual income:', () => {
     [
-        testCase(1, 0, 0),
         testCase(7998, 0, 0),
+        testCase(8040, 0, 0),
+        testCase(12000, 0, 0),
+        testCase(13200, 0, 0),
         testCase(29900, 0, 0),
-        testCase(70000, 4200, 1344),
+        testCase(30001, 0, 0),
+        testCase(36000, 2160, 259),
+        testCase(70000, 4200, 504),
+        testCase(84960, 5098, 612),
+        testCase(86400, 5184, 622),
+        testCase(100000, 6000, 720),
+        testCase(100001, 6000, 720),
+        testCase(120000, 7200, 864),
+        testCase(120001, 7200, 2304),
+        testCase(132000, 7920, 2534),
         testCase(168000, 10080, 3226),
         testCase(254000, 15240, 4877),
     ].forEach((test) => {
