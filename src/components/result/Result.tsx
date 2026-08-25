d6dbefff5e9829f2760d50147a2af61a6f23b21a
zyzik1248 <zuzannadudzinska96@gmail.com>
2026-08-25 15:15:32 +0200

    add reslut to who

--- src/components/result/Result.tsx
+++ src/components/result/Result.tsx
@@ -6,7 +6,7 @@ function Result() {
             <div className="first-result-who">
                 <div className="icon">%</div>
                 <p className="text text-1">Odpowiedz, czy rozliczasz się jako osoba prywatna (PIT) czy firma (CIT), a dowiesz się, z jakiego limitu odliczenia darowizn od dochodu możesz skorzystać!</p>
-                <p className="text text-2">Nic nie zapisujemy. Wyliczenie zostaje na Twoim ekranie.</p>
+                <p className="text text-2">Nic nie zapisujemy.<span> Wyliczenie zostaje na Twoim ekranie.</span></p>
             </div>
         </div>
     )
