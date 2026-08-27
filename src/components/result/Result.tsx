53ff26e3f446f6287a7ecd568b4b8d6b83b25b40
zyzik1248 <zuzannadudzinska96@gmail.com>
2026-08-27 11:38:53 +0200

    add cit

--- src/components/result/Result.tsx
+++ src/components/result/Result.tsx
@@ -40,7 +40,27 @@ function Result({ step, values, setStep, setValues }: ResultProps) {
         }
     ]
 
-    console.log(step == 2 && values.pit && !values.pitType)
+    const greenResults = [
+        {
+            btn: "Twoje dochody",
+            text: "Rozliczając się na skali podatkowej możesz skorzystać z odliczenia. Przejdź dalej, by podać swoje dochody i policzyć Twój limit darowizn na 2026 rok."
+        },
+        {
+            btn: "Twój przychód",
+            text: "Spółki rozliczające CIT mogą odliczyć darowiznę od dochodu, z limitem 10%. Przy stawce 19% każde odliczone 100 zł to 19 zł niższego podatku.",
+            addText: "Przejdź dalej, by podać roczny dochód firmy."
+        },
+        {
+            btn: "Dochód firmy",
+            text: "Spółki rozliczające CIT mogą odliczyć darowiznę od dochodu, z limitem 10%. Przy stawce 19% każde odliczone 100 zł to 19 zł niższego podatku.",
+            addText: "Przejdź dalej, by podać roczny dochód firmy."
+        },
+        {
+            btn: "Dochód firmy",
+            text: "Spółki rozliczające CIT mogą odliczyć darowiznę od dochodu, z limitem 10%. Przy stawce 9% każde odliczone 100 zł to 9 zł niższego podatku.",
+            addText: "Przejdź dalej, by podać roczny dochód firmy."
+        },
+    ]
 
     return (
         <div className="result" style={{ height }}>
@@ -57,16 +77,22 @@ function Result({ step, values, setStep, setValues }: ResultProps) {
                 <EmptyResult {...emptyResults[1]} />
             </div>
             <div ref={step == 2 && values.pit && values.pitType == "scale" ? ref : undefined} className={`result-card result-card-5 ${step == 2 && values.pit && values.pitType == "scale" ? "active" : ""}`}>
-                <GreenResult setStep={setStep} text="Rozliczając się na skali podatkowej możesz skorzystać z odliczenia. Przejdź dalej, by podać swoje dochody i policzyć Twój limit darowizn na 2026 rok." />
+                <GreenResult setStep={setStep} {...greenResults[0]} />
             </div>
             <div ref={step == 2 && values.pit && values.pitType == "lumpSum" && !values.lumpSum ? ref : undefined} className={`result-card result-card-5 ${step == 2 && values.pit && values.pitType == "lumpSum" && !values.lumpSum ? "active" : ""}`}>
                 <EmptyResult {...emptyResults[2]} />
             </div>
             <div ref={step == 2 && values.pit && values.pitType == "lumpSum" && values.lumpSum ? ref : undefined} className={`result-card result-card-5 ${step == 2 && values.pit && values.pitType == "lumpSum" && values.lumpSum ? "active" : ""}`}>
-                <GreenResult setStep={setStep} text='Ryczałt też pozwala odliczyć darowiznę — od przychodu, z limitem 6%. Przy stawce 12% każde odliczone 100 zł to 12 zł niższego podatku. Przejdź dalej, by podać swój przychód.' />
+                <GreenResult setStep={setStep} {...greenResults[1]} />
             </div>
             <div ref={step == 2 && values.cit && !values.citType ? ref : undefined} className={`result-card result-card-6 ${step == 2 && values.cit && !values.citType ? "active" : ""}`}>
                 <EmptyResult {...emptyResults[3]} />
+            </div>
+            <div ref={step == 2 && values.cit && values.citType == "cit19" ? ref : undefined} className={`result-card result-card-5 ${step == 2 && values.cit && values.citType == "cit19" ? "active" : ""}`}>
+                <GreenResult setStep={setStep} {...greenResults[2]} />
+            </div>
+            <div ref={step == 2 && values.cit && values.citType == "cit9" ? ref : undefined} className={`result-card result-card-5 ${step == 2 && values.cit && values.citType == "cit9" ? "active" : ""}`}>
+                <GreenResult setStep={setStep} {...greenResults[3]} />
             </div>
         </div>
     )
