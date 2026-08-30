6e111b17bf1b8f5c7b3b95cea042149a3b18be7b
zyzik1248 <zuzannadudzinska96@gmail.com>
2026-08-30 21:37:21 +0200

    fix

--- src/components/UI/hint/HintMessage.test.tsx
+++ src/components/UI/hint/HintMessage.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { describe, it, expect, vi } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import HintMessage from './HintMessage';
 
 describe("HintMessage", () => {
